0ed5972e5cff22fcee0e2445104fca35997c8407
Elieth Umaña <izaduartes2214@gmail.com>
2024-09-12 11:07:50 -0600

    inicio

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button,  TouchableHighlight } from 'react-native';
 
 export default function App() {
+
+  const [count, setCount] = useState(0);
+
+  const contador = () => {
+    setCount(count + 1)
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}>      
+      <TouchableHighlight
+        style={{borderColor:'blue',borderWidth:1, height:40, justifyContent:'center'}}
+        onPress={()=>Alert.alert('Haz presionado el boton')}>
+        <Text>Presiona aqui</Text>
+        </TouchableHighlight>
+      <Text style={{ fontSize: 24, marginBottom:20}}>Contador: {count}</Text>
+      <Button tittle="Incrementar " onPress={contador}/>
       <StatusBar style="auto" />
     </View>
   );
